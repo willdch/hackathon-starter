017ab769d67c4a995045a631704301df510494e1
Will Harris <will@lifenet.net>
2021-05-13 20:00:30 -0700

    updated ip address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.84.243.53',
+      host: '54.243.17.36',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
