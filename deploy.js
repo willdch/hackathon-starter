97fc8ad06856dc1270e77d81c51fc4393f389be2
Will Harris <will@lifenet.net>
2021-05-13 18:55:17 -0700

    updated ip address for aws instace

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.90.219.249',
+      host: '3.84.243.53',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
